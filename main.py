4550e4291f2d9d8b5e9eb356ec415158c66578b4
Sensoz <sensoz@gmail.com>
2012-02-16 16:03:47 +0700

    working on title screen

--- main.py
+++ main.py
@@ -1,39 +1,15 @@
-import curses
 
+from screen import Screen
 from engine import Engine
 
-class Screen:
-	def __init__(self, stdscr):
-		self.screen = stdscr
-		self.height = 40
-		self.width = 80
-		self.curx, self.cury = 0, 0
+def main():
+	screen = Screen()
+	screen.title_menu(game.menus['title_menu'])
 	
-	def catch_key(self, menu_keys):
-		while True:
-			key = self.screen.getch()
-			# depending on menu, process keys and run callbacks
-	
-	def title_menu(self):
-		# get the title menu from the config
-		# display it
-		self.catch_key()
-		
-	def main_loop(self):
-		# Find in which menu the player is and
-		# lookup the corresponding valid keys
-		self.catch_key()
-
-
-def main(stdscr):
-	screen = Screen(stdscr)
-	screen.title_menu()
-	
-
 
 if __name__ == '__main__':
 	# Load config and all menus in memory
 	game = Engine()
-	
-	
+	game.load_menus()
+
 	curses.wrapper(main)
