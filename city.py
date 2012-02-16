908d5fc1e20737f35a3978c59c4020ee3e45ebec
Senso <sensoz@gmail.com>
2012-02-17 00:10:48 +0700

    added initial city/district generation file

---
+++ city.py
@@ -0,0 +1,20 @@
+
+class City:
+	def __init__(self):
+		self.name = ''
+		self.districts = {}
+		
+	def generate_name(self):
+		pass
+	
+
+class District:
+	def __init__(self):
+		self.name = ''
+		self.population = 0
+		self.crime_rate = 0.0
+		self.median_income = 0
+		self.poverty_level = 0.0
+		
+	def generate_name(self):
+		pass
