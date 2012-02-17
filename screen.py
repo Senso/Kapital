250e19bdbd21f1afb39475202cc5ffa410b85717
Sensoz <sensoz@gmail.com>
2012-02-17 16:05:42 +0700

    split main window in two

--- screen.py
+++ screen.py
@@ -1,3 +1,4 @@
+from curses import newwin
 
 class Screen:
 	def __init__(self, stdscr):
@@ -6,7 +7,13 @@ class Screen:
 		self.width = 80
 		self.curx, self.cury = 0, 0
 		self.screen.resize(self.height, self.width)
-		self.screen.refresh()
+		
+		self.dat_scr = newwin(20, 80, 0, 0)
+		self.opt_scr = newwin(10, 80, 20, 0)
+		
+		self.dat_scr.refresh()
+		self.opt_scr.refresh()
+		#self.screen.refresh()
 	
 	def catch_key(self, opts):
 		# depending on menu, process keys and run callbacks
@@ -21,15 +28,18 @@ class Screen:
 	def display_menu(self, menu):
 		# get the title menu from the config
 		# display it
-		self.screen.clear()
+		#self.screen.clear()
+		self.dat_scr.clear()
+		self.opt_scr.clear()
+		
 		self.set_borders()
 		self.screen.addstr(2, 2, menu['title'])
 		offset = 3
 		
 		for d in menu['data'].items():
 			label = d[0]
-			val = d[1][0]
-			self.screen.addstr(offset + 1, 5, "%s: %s" % (label, val))
+			val = d[1]
+			self.dat_scr.addstr(offset + 1, 5, "%s: %s" % (label, val))
 			offset += 1
 		
 		offset += 2
@@ -37,9 +47,11 @@ class Screen:
 		for option in menu['options'].items():
 			o_key = option[0]
 			o_str = option[1][0]
-			self.screen.addstr(offset + 1, 5, "(%s) %s" % (o_key, o_str))
+			self.opt_scr.addstr(offset + 1, 5, "(%s) %s" % (o_key, o_str))
 			offset += 1
-		self.screen.refresh()
+			
+		self.dat_scr.refresh()
+		self.opt_scr.refresh()
 		
 		#callback = self.catch_key(menu['options'])
 		#return callback
